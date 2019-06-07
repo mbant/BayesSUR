7255dadd0a5e2872729c4e4b7cde25a8c1c8fa73
zhizuio <39630023+zhizuio@users.noreply.github.com>
2019-06-07 14:23:24 +0200

    Delete BayesSUR.R

--- BayesSUR/R/BayesSUR.R
+++
@@ -1,8 +0,0 @@
-#' BayesSUR -- Bayesian Seemingly Unrelated Regression
-#' @title Bayesian Sparse Seemingly Unrelated Regression
-#' @docType package
-#' @name BayesSUR
-#' @importFrom Rcpp sourceCpp
-#' @importFrom utils read.table write.table
-#' @useDynLib BayesSUR
-NULL
