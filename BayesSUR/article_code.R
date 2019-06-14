926d3c5cd77af862d6ebdf76fb8545f011f5bb47
zhizuio <39630023+zhizuio@users.noreply.github.com>
2019-06-14 10:05:13 +0200

    Add files via upload

--- BayesSUR/article_code.R
+++ BayesSUR/article_code.R
@@ -1,12 +1,12 @@
 library(BayesSUR)
-library(igraph)
-data(example_data, package = "BayesSUR")
-str(example_data)
+data(example_eQTL, package = "BayesSUR")
+str(example_eQTL)
 # show the simulated gamma matrix and G_0
+attach(example_eQTL)
 layout(matrix(1:2, ncol=2))
-image(z=example_data$gamma, x=1:150, y=1:10, col=grey(1:0), xlab="SNPs Index", 
+image(z=gamma, x=1:150, y=1:10, col=grey(1:0), xlab="SNPs Index", 
       ylab="Responses", main=mtext(bquote(True~" "~gamma)));box()
-image(z=t(example_data$G), x=1:10, y=1:10, col=grey(1:0), xlab="Responses", 
+image(z=t(G0), x=1:10, y=1:10, col=grey(1:0), xlab="Responses", 
       ylab="Responses", main="True graph of responses");box()
  
 #hyperpar = list(mrf_e=-3, mrf_d=3/10, b_pi = 0.2 , a_pi = 0.1 , b_o = 5 , a_o = 1 )
@@ -17,25 +17,29 @@ image(z=t(example_data$G), x=1:10, y=1:10, col=grey(1:0), xlab="Responses",
 #mean_pi * mean_o
 #sum(example_data$gamma)/prod(dim(example_data$gamma)); sum(example_data$gamma)
 
-fit <- runSUR(data = example_data$data, Y = example_data$blockList[[1]],
-              X = example_data$blockList[[2]], outFilePath = "results/", 
-              nIter = 1000, nChains = 4, covariancePrior = "HIW", 
+fit <- runSUR(data = data, Y = blockList[[1]],
+              X = blockList[[2]], outFilePath = "results/", 
+              nIter = 3000, nChains = 5, covariancePrior = "HIW", 
               gammaPrior = "hotspot")
-str(fit)
+
+str(summary(fit))
+# show the interaction of plots
+plot(fit)
 # show the estimated beta, gamma and G_0
 plotEstimator(fit);hat_gamma=getEstimator(fit);sum(hat_gamma>.5);max(hat_gamma)
 # show the relationship of responses
 #pdf("ResponseGraph.pdf", height=4, width=7)
-plotResponseGraph(fit, PtrueResponse=example_data$G0, 
-                  response.name=paste("GEX",1:ncol(example_data$G0),sep=""))
+plotResponseGraph(fit, PtrueResponse=G0, response.name=paste("GEX",1:ncol(G0),sep=""))
 #dev.off()
 # show the network representation of the associations between responses and features
-plotNetwork(fit, PmaxPredictor=0.8, lineup=1.2)
+pdf("ResponseNetwork7.pdf", height=10, width=10)
+plotNetwork(fit, PmaxPredictor=0.5, lineup=1.5, nodesizePredictor=5, nodesizeResponse=10)
+dev.off()
 # show the manhattan plot
-manhattan(fit)
+plotManhattan(fit)
 
 # check the convergence of the algorithm
-mcmcDiag(fit)
+plotMCMCdiag(fit)
 
 
 #=============
@@ -46,7 +50,7 @@ fit <- runSUR(data = example_GDSC$data,
               Y = example_GDSC$blockList[[1]],
               X = example_GDSC$blockList[[3]],
               X_0 = example_GDSC$blockList[[2]],
-              outFilePath = "results/", nIter = 2000, nChains = 2, 
+              outFilePath = "results/", nIter = 200, nChains = 2, 
               covariancePrior = "HIW", gammaPrior = "MRF", mrfG=example_data$mrfG)
 # show the estimated beta, gamma and G_0
 hat.gamma <- getEstimator(fit, "gamma")
@@ -60,9 +64,19 @@ pdf("ResponseEstimator.pdf", height=8, width=8);plotEstimator(fit);dev.off()
 # show the relationship of responses
 pdf("ResponseNetwork0.pdf", height=8, width=8);plotResponseGraph(fit);dev.off()
 # show the network representation of the associations between responses and features
-pdf("ResponseNetwork1.pdf", height=8, width=10);plotNetwork(fit, PmaxCovariate=0.5);dev.off()
-pdf("ResponseNetwork2.pdf", height=8, width=10);plotNetwork(fit, PmaxCovariate=0.6);dev.off()
-pdf("ResponseNetwork3.pdf", height=8, width=10);plotNetwork(fit, PmaxCovariate=0.7);dev.off()
+pdf("ResponseNetwork1.pdf", height=8, width=10);plotNetwork(fit, PmaxPredictor=0.5);dev.off()
+pdf("ResponseNetwork2.pdf", height=8, width=10);plotNetwork(fit, PmaxPredictor=0.6);dev.off()
+pdf("ResponseNetwork3.pdf", height=8, width=10);plotNetwork(fit, PmaxPredictor=0.7);dev.off()
+pdf("ResponseNetwork4.pdf", height=8, width=10);plotNetwork(fit, PmaxPredictor=0.5, excludeResponse="X17.AAG");dev.off()
+pdf("ResponseNetwork5.pdf", height=8, width=10);plotNetwork(fit, PmaxPredictor=0.5, excludeResponse="Methotrexate",includePredictor=names(read.table("targetGenes.txt",header=T)));dev.off()
+pdf("ResponseNetwork6.pdf", height=8, width=8)
+plotNetwork(fit, PmaxPredictor=0.5, includeResponse=c("RDEA119","PD.0325901","CI.1040","AZD6244"),includePredictor=names(read.table("targetGeneGroup1.txt",header=T)),
+            lineup=1.5,nodesizePredictor=5,nodesizeResponse=25)
+dev.off()
+pdf("ResponseNetwork7.pdf", height=5, width=5)
+plotNetwork(fit, PmaxPredictor=0.5, includeResponse=c("Nilotinib","Axitinib"),includePredictor=names(read.table("targetGeneGroup2.txt",header=T)),
+            lineup=1.5,nodesizePredictor=15,nodesizeResponse=25)
+dev.off()
 # show the manhattan plot
 #manhattan(fit)
 
