f0ff195ba7f24e1fae30c4735c040cc791e76b22
zhizuio <39630023+zhizuio@users.noreply.github.com>
2019-07-02 16:22:49 +0200

    Update test.R

--- test.R
+++ test.R
@@ -1,10 +1,10 @@
-## Build a new version of the package
-remove.packages("BayesSUR")
-Rcpp::compileAttributes(pkgdir = "/Users/zhiz/Downloads/BayesSUR/BayesSUR/"); devtools::document("/Users/zhiz/Downloads/BayesSUR/BayesSUR")
-devtools::build("/Users/zhiz/Downloads/BayesSUR/BayesSUR")#,vignettes=TRUE)
+### Build a new version of the package
+#remove.packages("BayesSUR")
+#Rcpp::compileAttributes(pkgdir = "/Users/zhiz/Downloads/BayesSUR/BayesSUR/"); devtools::document("/Users/zhiz/Downloads/BayesSUR/BayesSUR")
+#devtools::build("/Users/zhiz/Downloads/BayesSUR/BayesSUR")#,vignettes=TRUE)
 
 ## Install the package
-install.packages("/Users/zhiz/Downloads/BayesSUR/BayesSUR_0.1.7.tar.gz",repos = NULL,type = "source")
+install.packages("BayesSUR_0.1.13.tar.gz",repos = NULL,type = "source")
 
 
 #####################################################################################################
@@ -13,14 +13,14 @@ library(BayesSUR)
 data(example_eQTL, package = "BayesSUR")
 str(example_eQTL)
 
-# show the simulated gamma matrix and G_0
+# show the simulated gamma matrix and G_y
 attach(example_eQTL)
 options(tikzMetricPackages = c("\\usepackage{amsmath}","\\usepackage{bm}", "\\usetikzlibrary{calc}"))
 tikz('ParamTrue.tex',width=5.5,height=3, standAlone = TRUE,packages = c("\\usepackage{tikz}","\\usepackage{amsmath}","\\usepackage{bm}"))
 layout(matrix(1:2, ncol=2))
 image(z=gamma, x=1:150, y=1:10, col=grey(1:0), xlab="SNPs Index", 
       ylab="Responses", main=paste("True","$\\bm{\\gamma}$"));box()
-image(z=t(G0), x=1:10, y=1:10, col=grey(1:0), xlab="Responses", 
+image(z=t(Gy), x=1:10, y=1:10, col=grey(1:0), xlab="Responses", 
       ylab="Responses", main=paste("True","$\\mathcal{G}$"));box()
 dev.off()
 tools::texi2pdf("ParamTrue.tex")
@@ -37,11 +37,11 @@ str(summary(fit))
 # show the interaction of plots
 plot(fit)
 
-# show the estimated beta, gamma and G_0
+# show the estimated beta, gamma and G_y
 plotEstimator(fit);
 
 # show the relationship of responses
-plotResponseGraph(fit, PtrueResponse=G0, response.name=paste("GEX",1:ncol(G0),sep=""))
+plotResponseGraph(fit, PtrueResponse=Gy, response.name=paste("GEX",1:ncol(Gy),sep=""))
 
 # show the network representation of the associations between responses and features
 plotNetwork(fit,label.predictor = NA,lineup=1.5,nodesizePredictor=2,nodesizeResponse=15,
